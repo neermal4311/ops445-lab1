571d9e21722d103701d203bc1d928d19986b74a3
Nirmal Gautam <neermal4311@gmail.com>
2024-09-12 21:56:59 -0400

    Added lab1d script

---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Description: This script performs basic arithmetic operations and prints the result.'''
+
+# Define the variables
+x = 10
+y = 2
+z = 5
+
+# Perform the arithmetic operation
+result = x + y * z
+
+# Print the result in the specified format
+print(f'{x} + {y} * {z} = {result}')
