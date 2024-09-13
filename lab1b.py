bcef732e94bceea5075a62f84e2acd359c51a0a4
Nirmal Gautam <neermal4311@gmail.com>
2024-09-12 21:53:10 -0400

    Added lab1b and lab1c scripts

---
+++ lab1b.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Nirmal Gautam - ngautam11 - OPS445ZAA
+
+name = "Isaac"
+
+print('How old are you ' + name + '?')
---
+++ lab1c.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''Description: This script prints a message with an integer value.'''
+
+# Define integer objects
+num1 = 5
+num2 = 10
+
+# Calculate sum
+sum_value = num1 + num2
+
+# Print the sum
+print(f'The sum is: {sum_value}')
+
+# Define additional objects
+name = 'Isaac'
+age = 72
+
+# Print a message including both string and integer objects
+print(f'{name} is {age} years old!')
+
